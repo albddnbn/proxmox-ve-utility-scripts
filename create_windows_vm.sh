333f510cd4d40dea1791149410c2ccf4528f5fd6
Alex B. <105134673+albddnbn@users.noreply.github.com>
2024-07-23 20:58:01 -0400

    Create create_windows_vm.sh

---
+++ create_windows_vm.sh
@@ -0,0 +1,2 @@
+## OFfers user a range of interactive options and generates a Windows VM with recommended hardware settings.
+## **Optionally creates corresponding firewall aliases and software-defined networking elements for VM.
