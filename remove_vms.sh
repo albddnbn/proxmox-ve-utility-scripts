1dbaa02e99f8ad8af1e1647e210726ac2696349a
Alex B. <abuddenb@gmail.com>
2024-07-26 21:33:44 -0400

    Remove_vms.sh is functional - allows removal of qm and lxc objects.

--- remove_vms.sh
+++ remove_vms.sh
@@ -1,19 +1,36 @@
-## Presents list of all VMs - user checks off ones to remove.
-## Script really needs to grab id column from the api endpoint so we can differentiate between containers/VMs/etc.
-## ^ Right now, I only know that pct destroy looks for container in nodes/NODE/lxc/VMID.conf
-## qm destroy likely checks in nodes/NODE/qemu/VMID.conf
+#!/bin/bash
+# Script Name: remove_vms.sh
+# Author: Alex B.
+# Date: 7/26/2024
+# Description: Allows user to check off the VMs/containers they'd like to be purged from the cluster. Depending on whether
+#              the container is qemu or lxc, the qm or pct utility is used (respectively).
 source functions.sh
 
 VMS_TO_REMOVE=$(create_checklist -b "Select VMs to remove:" --title "Select VMs to remove:" --pvesh "pvesh get /cluster/resources --type vm --noborder --output-format json" -mc "vmid" -sc "name")
 dialog --clear
 for single_vm in $VMS_TO_REMOVE; do
-    # echo "Removing VM: $single_vm"
-    ## use pct to force shutdown the vm then destroy it:
-    # pct shutdown $single_vm -forceStop 1
 
-    ## destroy the vm
-    qm destroy $single_vm -purge 2>/dev/null &
-    pid=$! # Process Id of the previous running command
-    run_spinner $pid "Removing VM: $single_vm"
+    ## this loop cycles through the VM IDs that user has chosen to delete.
+    ## the id property of /cluster/resources --type vm holds whether a VM is qemu or lxc
+    mapfile -t pve_api_listing <<< $(eval "pvesh get /cluster/resources --type vm --output json" | jq -r ".[] | .id" | grep "$single_vm")
+
+    container_type=$(echo $pve_api_listing | cut -d '/' -f 1)
+
+    # read -p "Container type: $container_type"
+
+    if [[ $container_type == "qemu" ]]; then
+        ## destroy the vm
+        qm destroy $single_vm -purge 2>/dev/null &
+        pid=$! # Process Id of the previous running command
+        run_spinner $pid "Removing VM: $single_vm"
+
+    elif [[ $container_type == "lxc" ]]; then
+        echo "Removing LXC: $single_vm"
+        pct destroy $single_vm -force -purge 2>/dev/null &
+        pid=$! # Process Id of the previous running command
+        run_spinner $pid "Removing VM: $single_vm"
+    else
+        echo "Unknown container type: $container_type"
+    fi
 
 done
