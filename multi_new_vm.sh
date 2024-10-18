ccce5c3100c6e7b14b5cfc67468e3bc45ee1d353
Alex B. <105134673+albddnbn@users.noreply.github.com>
2024-10-17 21:44:35 -0400

    Tested, should be able to replace new_vm after more thorough testing. Creates multiple VMs with the same settings, incrememts vm names and IDs
    Can hopefully replace the new_vm script with a bit of work. Creates multiple VMs with the same settings, increments vm names and IDs
    
    Allows selection of two isos for VM(s), joining to network, etc.

--- multi_new_vm.sh
+++ multi_new_vm.sh
@@ -4,14 +4,12 @@
 # Author: Alex B.
 # Date: 2024-07-28
 # Description: Creates new Proxmox VMs in a loop - increments given hostname by one.
-# Usage: ./new_vm.sh
+# *Hoping this will just replace the new_vm script eventually..
+# Usage: ./multi_new_vm.sh
 # Notes:
-# - Virtual machine hardware settings derived from 'known good' recommendations:
-#     https://4sysops.com/archives/create-a-windows-vm-in-proxmox-ve/
-#     https://davejansen.com/recommended-settings-windows-10-2016-2018-2019-vm-proxmox/
+# - Search for available VM IDs seems like it reaally could use some improvement.
 #
 # ---------------------------------------------------------------------------------------------------------------------
-
 ########################################################################################################################
 ## Stage 1 - Preparation
 ## Sourcing functions file, defining associative arrays, ensuring values are set, prompting user when necessary.
@@ -24,46 +22,48 @@ for file in $(ls "$script_dir/functions/"*".sh"); do
     source "$file"
 done
 
+
+## BASIC VIRTUAL MACHINE SETTINGS:
+## Starting vm id - script will start creating VMs at this id and increment upwards as available.
+## Hostname prefix - prefix for VM hostnames, a number is appended. If it's a single digit, it has a 0 before it.
 declare -A VM_SETTINGS=(
     ## Details for VM creation:
-    ["VM_ID"]="100"                                      # Ex: 101
-    ["HOSTNAME_PREFIX"]="a-pc-"                              # Ex: lab-dc-01
+    ["STARTING_VM_ID"]="100"                             # Ex: 101
+    ["HOSTNAME_PREFIX"]="a-pc-"                          # Ex: lab-pc-
     ["NUM_CORES"]=1                                      # Number of CPU cores used by VM
     ["NUM_SOCKETS"]=1                                    # Number of CPU sockets used by VM
     ["MEMORY"]=8192                                      # VM Memory in GB
     ["VM_NETWORK"]=""                                    # Network for VM
     ["VM_HARDDISK_SIZE"]="60"                            # Ex: 60 would create a 60 GB hard disk.
+    ["NUMBER_VMS"]=2                                     # Number of VMs to create
 )
 
-## choose number of virtual machines
-NUMBER_VMS=$(create_text_entry -t "Number of Virtual Machines" -s "Enter number of virtual machines to create:")
-STARTING_VM_ID=$(create_text_entry -t "Starting VM ID" -s "Enter starting VM ID:")
-## enter starting hostname
-${VM_SETTINGS['HOSTNAME_PREFIX']}=$(create_text_entry -t "Enter hostname prefix (number will be appeneded to end):" -s "${VM_SETTINGS['HOSTNAME_PREFIX']}")
-
-
-# Store data to $VALUES variable
+# Store data to $VALUES variable and present is as form
 VALUES=$(dialog --ok-label "Submit" \
     --backtitle "Settings Verification" \
     --title "Verify Virtual Machine Settings" \
     --form "Please correct values as necessary:" \
     25 80 0 \
-    "Hostname Prefix:"     2  1	"${VM_SETTINGS['HOSTNAME_PREFIX']}" 	          2  25 35 0 \
-    "Virtual Machine Memory:"   3  1	"${VM_SETTINGS['MEMORY']}" 	            3  25 35 0 \
-    "Virtual Machine Cores:"    4  1	"${VM_SETTINGS['NUM_CORES']}" 	        4  25 35 0 \
-    "Virtual Machine Sockets:"  5  1	"${VM_SETTINGS['NUM_SOCKETS']}" 	      5  25 35 0 \
-    "Hard Disk Size:"           6  1	"${VM_SETTINGS['VM_HARDDISK_SIZE']}"  	6  25 35 0 \
+    "Starting VM ID:"     1  1	"${VM_SETTINGS['STARTING_VM_ID']}" 	          1  25 35 0 \
+    "Number of VMs"     2  1	"${VM_SETTINGS['NUMBER_VMS']}" 	          2  25 35 0 \
+    "Hostname Prefix:"     3  1	"${VM_SETTINGS['HOSTNAME_PREFIX']}" 	          3  25 35 0 \
+    "Virtual Machine Memory:"   4  1	"${VM_SETTINGS['MEMORY']}" 	            4  25 35 0 \
+    "Virtual Machine Cores:"    5  1	"${VM_SETTINGS['NUM_CORES']}" 	        5  25 35 0 \
+    "Virtual Machine Sockets:"  6  1	"${VM_SETTINGS['NUM_SOCKETS']}" 	      6  25 35 0 \
+    "Hard Disk Size:"           7  1	"${VM_SETTINGS['VM_HARDDISK_SIZE']}"  	7  25 35 0 \
 3>&1 1>&2 2>&3 3>&-)
 
 ## turn $VALUES variable into an array
 mapfile -t vm_setting_choices <<< "$VALUES"
 
 ## Reassign values to VM_SETTINGS array
-VM_SETTINGS["HOSTNAME_PREFIX"]="${vm_setting_choices[0]}"
-VM_SETTINGS["MEMORY"]="${vm_setting_choices[1]}"
-VM_SETTINGS["NUM_CORES"]="${vm_setting_choices[2]}"
-VM_SETTINGS["NUM_SOCKETS"]="${vm_setting_choices[3]}"
-VM_SETTINGS["VM_HARDDISK_SIZE"]="${vm_setting_choices[4]}"
+VM_SETTINGS["STARTING_VM_ID"]="${vm_setting_choices[0]}"
+VM_SETTINGS["NUMBER_VMS"]="${vm_setting_choices[1]}"
+VM_SETTINGS["HOSTNAME_PREFIX"]="${vm_setting_choices[2]}"
+VM_SETTINGS["MEMORY"]="${vm_setting_choices[3]}"
+VM_SETTINGS["NUM_CORES"]="${vm_setting_choices[4]}"
+VM_SETTINGS["NUM_SOCKETS"]="${vm_setting_choices[5]}"
+VM_SETTINGS["VM_HARDDISK_SIZE"]="${vm_setting_choices[6]}"
 
 ## Select node name (node is auto-selected if there's only one)
 NODE_NAME=$(user_selection_single -b "Node Selection" -t "Please select node:" -p "pvesh get /nodes --output json" -c "node" -a "1")
@@ -87,9 +87,11 @@ dialog --clear
 ## Storage locations include:
 ## - ISO_STORAGE: Storage location for Windows and VirtIO ISOs
 ## - VM_STORAGE: Storage location for VM hard disk
+##
 ## User is prompted to select TWO ISOs.
 ## - main_iso: Windows ISO
 ## - virtio_iso: VirtIO ISO
+##
 ########################################################################################################################
 
 ## Prompt user for STORAGE_OPTIONS values
@@ -114,7 +116,7 @@ VM_SETTINGS["VM_NETWORK"]=$vm_network_reply
 NETWORK_ADAPTER_TYPE="e1000" # Some example options include: e1000, virtio e1000e. There are likely options for Realtek and VMWare adapters as well.
 
 ## cycle through num_vms:
-for i in $(seq $NUMBER_VMS); do
+for i in $(seq ${VM_SETTINGS['NUMBER_VMS']}); do
 
     ## if i is 1-9 - prepend a 0
     if [ $i -lt 10 ]; then
@@ -124,9 +126,10 @@ for i in $(seq $NUMBER_VMS); do
   ## Create VM name
   virtual_machine_name="${VM_SETTINGS['HOSTNAME_PREFIX']}${i}"
 
+    ## This search for vm ids is very lengthy..
     vm_id_open="no"
     while [ "$vm_id_open" == "no" ]; do
-        vm_id_check=$(check_pve_item -p "pvesh get /cluster/resources --type vm --output json" -s "$STARTING_VM_ID" -c "id")
+        vm_id_check=$(check_pve_item -p "pvesh get /cluster/resources --type vm --output json" -s "${VM_SETTINGS['STARTING_VM_ID']}" -c "id")
         vm_ids_separated=()
         ## Separate out the ID #s using cut -d '/' -f 2
         ## the items originally look like 'qemu/101' or 'lxc/102' so we have to chop off the 'container type'
@@ -135,28 +138,26 @@ for i in $(seq $NUMBER_VMS); do
         done;
 
         ## Check vm_ids_separated for exact match of VARS[VM_ID]
-        exact_match=$(echo "${vm_ids_separated[@]}" | grep -ow "$STARTING_VM_ID")
+        exact_match=$(echo "${vm_ids_separated[@]}" | grep -ow "${VM_SETTINGS['STARTING_VM_ID']}")
         if [ -z "$exact_match" ]; then
             vm_id_open="yes"
         else
             ## Resource for the redirection part of the command below: https://stackoverflow.com/questions/29222633/bash-dialog-input-in-a-variable#29222709
-            echo "Setting new vm id: $STARTING_VM_ID"
-            new_vm_id=$((STARTING_VM_ID + 1))
-            STARTING_VM_ID=$new_vm_id
-            echo "New vm id: $STARTING_VM_ID"
+            echo "Setting new vm id: ${VM_SETTINGS['STARTING_VM_ID']}"
+            new_vm_id=$((${VM_SETTINGS['STARTING_VM_ID']} + 1))
+            VM_SETTINGS['STARTING_VM_ID']=$new_vm_id
+            echo "New vm id: ${VM_SETTINGS['STARTING_VM_ID']}"
         fi
 
         dialog --clear
     done
 
-
   ## Creates a vm using specified ISO(s) and storage locations.
   # Reference for 'ideal' VM settings: https://davejansen.com/recommended-settings-windows-10-2016-2018-2019-vm-proxmox/
-  pvesh create /nodes/$NODE_NAME/qemu -vmid $STARTING_VM_ID -name "$virtual_machine_name" -storage ${STORAGE_OPTIONS['ISO_STORAGE']} \
+  pvesh create /nodes/$NODE_NAME/qemu -vmid ${VM_SETTINGS['STARTING_VM_ID']} -name "$virtual_machine_name" -storage ${STORAGE_OPTIONS['ISO_STORAGE']} \
         -memory 8192 -cpu cputype=x86-64-v2-AES -cores 4 -sockets 1 -cdrom "${chosen_isos['main_iso']}" \
         -ide1 "${chosen_isos['virtio_iso']},media=cdrom" -net0 "$NETWORK_ADAPTER_TYPE,bridge=${VM_SETTINGS['VM_NETWORK']},firewall=1" \
         -scsihw virtio-scsi-pci -bios ovmf -machine pc-q35-8.1 -tpmstate "${STORAGE_OPTIONS['VM_STORAGE']}:4,version=v2.0," \
         -efidisk0 "${STORAGE_OPTIONS['VM_STORAGE']}:1" -bootdisk ide2 -ostype win11 \
         -agent 1 -virtio0 "${STORAGE_OPTIONS['VM_STORAGE']}:${VM_SETTINGS['VM_HARDDISK_SIZE']},iothread=1,format=qcow2" -boot "order=ide2;virtio0;scsi0"
-
 done
